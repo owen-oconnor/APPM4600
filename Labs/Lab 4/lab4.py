cad3ce498c1d225355f8f797ef4ee2ba42f7316d
owen-oconnor <owoc8913@colorado.edu>
2024-09-19 14:11:50 -0600

    added function to calc order of convergence

--- Labs/Lab 4/lab4.py
+++ Labs/Lab 4/lab4.py
@@ -16,7 +16,19 @@ def fixed_point(f, x0, tol, Nmax):
     err = 1
     return np.array(values), err
 
+def order_of_convergence(seq, p_true):
+    errors = np.abs(seq - p_true)  
+    orders = []
+    
+    for n in range(1, len(errors) - 1):
+        alpha = np.log(errors[n+1]/errors[n]) / np.log(errors[n]/errors[n-1])
+        orders.append(alpha) 
+    
+    return orders
+
 
 g = lambda x: math.sqrt(10 / (x+4))
 sequence, err = fixed_point(g, 1.5, 1e-10, 200)
 print(sequence)
+ord = order_of_convergence(sequence, p_true=1.3652300134140976)
+print(ord)
