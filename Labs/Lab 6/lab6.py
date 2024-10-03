2bcd49290d270974f1a8d80a1110bbb2a69d1fb4
owen-oconnor <owoc8913@colorado.edu>
2024-10-03 13:41:16 -0600

    created lab6 files and added lazy newton from class

---
+++ Labs/Lab 6/lab6.py
@@ -0,0 +1,69 @@
+import numpy as np
+import math
+from numpy.linalg import norm
+from numpy.linalg import inv
+
+def evalF(x): 
+
+    F = np.zeros(3)
+    
+    F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
+    F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
+    F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3
+    return F
+    
+def evalJ(x): 
+
+    
+    J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
+        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
+        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
+    return J
+
+
+def Newton(x0,tol,Nmax):
+
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    for its in range(Nmax):
+       J = evalJ(x0)
+       Jinv = inv(J)
+       F = evalF(x0)
+       
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier, its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]
+           
+def LazyNewton(x0,tol,Nmax):
+
+    ''' Lazy Newton = use only the inverse of the Jacobian for initial guess'''
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    J = evalJ(x0)
+    Jinv = inv(J)
+    for its in range(Nmax):
+
+       F = evalF(x0)
+       x1 = x0 - Jinv.dot(F)
+       
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return[xstar, ier,its]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return[xstar,ier,its]   
