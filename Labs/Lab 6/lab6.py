4e6a11b34f33d387b4f4c3a4bdc359b9caa19121
owen-oconnor <owoc8913@colorado.edu>
2024-10-03 15:11:57 -0600

    implemented slacker method with check on update to x0

--- Labs/Lab 6/lab6.py
+++ Labs/Lab 6/lab6.py
@@ -23,33 +23,15 @@ print(forward_diffs)
 print(centered_diffs)
 
 
-def evalF(x): 
-
-    F = np.zeros(3)
-    
-    F[0] = 3*x[0]-math.cos(x[1]*x[2])-1/2
-    F[1] = x[0]-81*(x[1]+0.1)**2+math.sin(x[2])+1.06
-    F[2] = np.exp(-x[0]*x[1])+20*x[2]+(10*math.pi-3)/3
-    return F
-    
-def evalJ(x): 
-
-    
-    J = np.array([[3.0, x[2]*math.sin(x[1]*x[2]), x[1]*math.sin(x[1]*x[2])], 
-        [2.*x[0], -162.*(x[1]+0.1), math.cos(x[2])], 
-        [-x[1]*np.exp(-x[0]*x[1]), -x[0]*np.exp(-x[0]*x[1]), 20]])
-    return J
-
-
-def Newton(x0,tol,Nmax):
+def Newton(f, j, x0, tol, Nmax):
 
     ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
     ''' Outputs: xstar= approx root, ier = error message, its = num its'''
 
     for its in range(Nmax):
-       J = evalJ(x0)
+       J = j(x0)
        Jinv = inv(J)
-       F = evalF(x0)
+       F = f(x0)
        
        x1 = x0 - Jinv.dot(F)
        
@@ -62,19 +44,19 @@ def Newton(x0,tol,Nmax):
     
     xstar = x1
     ier = 1
-    return[xstar,ier,its]
+    return [xstar, ier, its]
            
-def LazyNewton(x0,tol,Nmax):
+def LazyNewton(f, j, x0,tol,Nmax):
 
     ''' Lazy Newton = use only the inverse of the Jacobian for initial guess'''
     ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
     ''' Outputs: xstar= approx root, ier = error message, its = num its'''
 
-    J = evalJ(x0)
+    J = j(x0)
     Jinv = inv(J)
     for its in range(Nmax):
 
-       F = evalF(x0)
+       F = f(x0)
        x1 = x0 - Jinv.dot(F)
        
        if (norm(x1-x0) < tol):
@@ -86,4 +68,68 @@ def LazyNewton(x0,tol,Nmax):
     
     xstar = x1
     ier = 1
-    return[xstar,ier,its]   
+    return [xstar, ier, its]   
+
+def slacker_newton(f1, j, x0, tol, Nmax):
+
+    ''' Slacker Newton = use only the inverse of the Jacobian for initial guess'''
+    ''' inputs: x0 = initial guess, tol = tolerance, Nmax = max its'''
+    ''' Outputs: xstar= approx root, ier = error message, its = num its'''
+
+    J = j(x0)
+    Jinv = inv(J)
+    update_count = 0
+    for its in range(Nmax):
+
+       f = f1(x0)
+       x1 = x0 - Jinv.dot(f)
+       
+       if norm(-Jinv.dot(f)) > 1e-5: # update Jacobian if step is too big
+           J = j(x1)
+           Jinv = inv(J)
+           update_count += 1 # count how many times jacobian is updated  
+
+       if (norm(x1-x0) < tol):
+           xstar = x1
+           ier =0
+           return [xstar, ier, its, update_count]
+           
+       x0 = x1
+    
+    xstar = x1
+    ier = 1
+    return [xstar, ier, its]  
+
+
+'''Exercises'''
+
+# 3.2
+
+def func(x):
+    f = np.array([4*x[0]**2 + x[1]**2 - 4,
+                   x[0] + x[1] - np.sin(x[0] - x[1])])
+    return f
+
+def jacob(x): # calculate jacobian for f
+    j = np.array([[8*x[0], 2*x[1]],
+                  [1 - np.cos(x[0] - x[1]), 1 + np.cos(x[0] - x[1])]])
+    return j
+
+x0 = np.array([1, 
+               0])
+tol = 1e-10
+
+
+lazy = LazyNewton(func, jacob, x0, tol, 500)
+root = lazy[0]
+iters = lazy[2]
+print(f'The approx root with lazy newton is {root} in {iters} iterations')
+
+
+slacker = slacker_newton(func, jacob, x0, tol, 500)
+root = slacker[0]
+iters = slacker[2]
+updates = slacker[3]
+print(f'The approx root with slacker newton is {root} in {iters} iterations with {updates} updates to the Jacobian')
+
+# 3.3
