58d4780c7cde1d518ad66773e8255978bc71dde3
owen-oconnor <owoc8913@colorado.edu>
2024-10-17 14:00:38 -0600

    added files for lab8

---
+++ Homework/Homework 7/hw7.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy.linalg import inv
+import matplotlib.pyplot as plt
+
+'''Question 1'''
+
+lambda x: 1 / 1 + (10*x)**2
+
+xeval = np.linspace(-1, 1, 1001)
+
+
+'''Question 2'''
+
+
+
+'''Question 3'''
